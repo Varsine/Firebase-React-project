54d3e7a2be1f6f26a81b3880288ded7fece83a93
Varsine <varsulmozg@gmail.com>
2020-03-28 17:44:55 +0400

    add search folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./App.css"
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
-import Search from "./components/navbar/Search"
+import Search from "./components/navbar/search/Search"
 import logo from "./components/image/logo.png"
 import Dropdown from "./components/navbar/dropdown/Dropdown"
 
