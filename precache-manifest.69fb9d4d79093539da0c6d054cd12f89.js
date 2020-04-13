d0ee381f541d2af0342baf1395ac9f43c0cc6248
Varsine <varsulmozg@gmail.com>
2020-04-13 21:48:31 +0400

    Updates

---
+++ precache-manifest.69fb9d4d79093539da0c6d054cd12f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c02592ab0ba919edabed1c56b0ee50",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "82498c14688b043a6498",
+    "url": "/my-app/static/css/main.9b90e0b2.chunk.css"
+  },
+  {
+    "revision": "412c6d5ac1e40f35a4b8",
+    "url": "/my-app/static/js/2.308ffbcf.chunk.js"
+  },
+  {
+    "revision": "f2e98ec675f761af4dc07075046c2301",
+    "url": "/my-app/static/js/2.308ffbcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82498c14688b043a6498",
+    "url": "/my-app/static/js/main.e8a5b612.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  }
+]);
